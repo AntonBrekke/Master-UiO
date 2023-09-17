f5034a96ad48e3fa02700412856c7c9a41fc541d
AntonBrekke <anton.a@hotmail.no>
2023-09-17 19:50:14 +0200

    Made plot in QFT

--- 1.semester/FYS4170/sketch_QFT_ps3.py
+++ 1.semester/FYS4170/sketch_QFT_ps3.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 2
-z = np.linspace(-n*np.pi, n*np.pi, 10000)[:,None]
-a = np.linspace(0.1, 1, 3)[None,:]
+n = 20
 p0 = 5
+wavelength = 2*np.pi / p0
+z = np.linspace(-n*wavelength, n*wavelength, 10000)[:,None]
+dp = np.linspace(1e-2, 1e-1, 3)[None,:]
 
-def Psi(z, a):
-    return np.sqrt(a/np.pi)*np.cos(p0*z)*np.exp(-a*z**2)
+def Psi(z, dp):
+    return dp * np.sqrt(2/np.pi) * np.exp(1j*p0*z) * np.exp(-2*dp**2 * z**2)
 
-plt.plot(z, Psi(z,a), '--')
+plt.plot(z, np.real(Psi(z,dp)), alpha=0.8)
 plt.grid(True)
-plt.legend([f'a={i}' for i in a[0]])
+plt.xlabel('z', fontsize=16)
+plt.legend([f'$\Delta$p={i:.2f}' for i in dp[0]], prop={'size':12})
 plt.show()
