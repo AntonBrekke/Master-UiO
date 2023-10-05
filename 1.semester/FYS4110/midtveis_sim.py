01d689e4098059a06841738add70b19e8036be26
AntonBrekke <anton.a@hotmail.no>
2023-10-05 18:52:06 +0200

    Made changes and added comments to midtveis_sim.py

---
+++ 1.semester/FYS4110/midtveis_sim.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+"""
+Task 2g) midterm FYS4110
+Represent + measure as 1, - measure as -1 in code.
+"""
+
+def P(pm, eig):
+    # Probability for measuring either + or - 
+    return 1/2 * (1 + pm*np.imag(eig))      # P(\pm) = 1/2 * (1 \pm \sin(\theta))
+
+def iterate_circuit(N_iter, eig0):
+    eig = np.zeros(N_iter, dtype=complex)       # Contatining eigenvalue for each iteration 
+    eig[0] = eig0 
+    for n in range(0, N_iter-1):
+        pm = np.random.choice((-1,1), p=[P(-1, eig[n]), P(1, eig[n])])        # pm is either + or - 
+        eig[n+1] = eig[n] * np.exp(1j*(-1*pm * abs(ze)))        # -1*pm = mp (-1*+- = -+)
+    return eig 
+
+
+ze = 1e-3           # Smaller -> more iterations for convergence, less "noise"        
+theta0 = np.pi    # initial angle for eigenvalue 
+eig0 = np.exp(1j*theta0)        # Initial eigenvalue
+N_iter = int(1e4)        
+
+eig = iterate_circuit(N_iter, eig0)
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_title(r'$\theta = \pi,$' + r'$\quad|z||\epsilon|$ = ' + f'{ze:.0e}', fontsize=20)
+
+ax.plot(range(0, N_iter), eig.real, 'tab:blue')
+ax.plot(range(0, N_iter), eig.imag,'r')
+ax.plot(range(0, N_iter), abs(eig),'g')
+ax.set_xlabel('Number of iterations', fontsize=16)
+ax.legend(['Re(eig)', 'Im(eig)'], prop={'size':16})
+
+plt.show()
