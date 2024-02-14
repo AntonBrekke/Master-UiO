e246c541f7efde02e3aaeb004a14e7828d2d8253
AntonBrekke <anton.a@hotmail.no>
2024-02-14 15:52:11 +0100

    Dunoo

--- 2.semester/FYS4160/satellite_time_dilation.py
+++ 2.semester/FYS4160/satellite_time_dilation.py
@@ -18,5 +18,10 @@ def delta_time_tot(r_earth, r_sat, dt_earth=86400):
     return delta
 
 delta_time = delta_time_tot(R_earth, R_sat, dt_earth)
-print(f'\nTotal difference in time due to GR and SR: {delta_time:.3e}')
-print(f'Length scale given light speed signals: {delta_time*c:.3e}')
+print(f'\nTotal difference in time due to GR and SR: {delta_time:.3e} s')
+print(f'Length scale given light speed signals: {delta_time*c:.3e} m')
+
+# Say I want 15m precicion on GPS. How long will it take before it messes up?
+meter_precision = 15            # m
+dt_earth = meter_precision/(c*(1/gamma + np.sqrt((c**2 + 2*phi(R_sat))/(c**2 + 2*phi(R_earth))) - 2))
+print(f'Given 15m precicion on GPS, without relativic correction: {dt_earth:.2f} s')
